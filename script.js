f91c290de423b4147e0b608ec9c18637ee623a9e
yisroel gold <yisoelgold3@gmail.com>
2024-09-19 16:33:06 +0300

    made a function renderDiv every time you run it reloads the player cards from the localstorage added a button to add a team to the api

--- script.js
+++ script.js
@@ -8,13 +8,60 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const position = document.getElementById('selectPosition"');
-const Points = document.getElementById("inputTotalPoints");
-const threePointes = document.getElementById("input3Pointers");
-const twoPointers = document.getElementById("input2Pointers");
 const submitButton = document.getElementById('searchPlayers');
-const BASE_URL = "https://nbaserver-q21u.onrender.com/api/filter";
+const BASE_URL = "https://nbaserver-q21u.onrender.com/api/";
+// const Bonus_Url: string = "localhost:8000/api/AddTeam"
 submitButton.addEventListener('click', postPlayers);
+const addTeamButton = document.getElementById('addTeamButton');
+const positions = ["PG", "SG", "SF", "PF", "C"];
+addTeamButton === null || addTeamButton === void 0 ? void 0 : addTeamButton.addEventListener('click', addTeam);
+function apiMessage(msg) {
+    let div = document.getElementById("apiMessage");
+    if (msg == 200) {
+        // div.innerText = `${y}`
+    }
+    div.innerText = msg;
+    div.style.backgroundColor = "green";
+    setTimeout(function () {
+        div.innerHTML = '';
+        div.style.backgroundColor = '';
+    }, 1000);
+}
+function addTeam() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const teamPlayers = {
+            players: []
+        };
+        for (let i = 0; i < positions.length; i++) {
+            let data = getDataLocalStorage(positions[i]);
+            teamPlayers.players.push(data);
+        }
+        try {
+            let data = yield fetch(`${BASE_URL}AddTeam`, {
+                method: "POST",
+                headers: { 'content-Type': 'application/json' },
+                body: JSON.stringify(teamPlayers)
+            });
+            console.log(data);
+            let res = yield data.json();
+            renderPlayers(res);
+            apiMessage(data.status);
+            return res;
+        }
+        catch (err) {
+            console.log(err);
+        }
+        localStorage.clear();
+        renderDiVCards();
+    });
+}
+function getDataLocalStorage(position) {
+    let data = localStorage.getItem(position);
+    if (typeof data === 'string') {
+        const player = JSON.parse(data) || [];
+        return player;
+    }
+}
 function postPlayers(event) {
     return __awaiter(this, void 0, void 0, function* () {
         event === null || event === void 0 ? void 0 : event.preventDefault();
@@ -26,16 +73,16 @@ function postPlayers(event) {
             threePercent: arr[1],
             points: arr[0]
         };
-        console.log(searchFactors);
         try {
-            let data = yield fetch(BASE_URL, {
+            let data = yield fetch(`${BASE_URL}filter`, {
                 method: "POST",
                 headers: { 'content-Type': 'application/json' },
                 body: JSON.stringify(searchFactors)
             });
             let res = yield data.json();
-            console.log(res);
+            apiMessage(data.status);
             renderPlayers(res);
+            return res;
         }
         catch (err) {
             console.log(err);
@@ -56,6 +103,7 @@ function renderPlayers(players) {
     tableBody.innerHTML = "";
     if (players) {
         players.forEach(element => {
+            const player = {};
             let tr = document.createElement('tr');
             for (let i = 0; i < ALL_PROPERTIES.length; i++) {
                 let td = document.createElement('td');
@@ -66,45 +114,61 @@ function renderPlayers(players) {
                 else if (typeof val === 'number') {
                     td.innerText = val.toString();
                 }
+                player[ALL_PROPERTIES[i]] = val;
                 tr.appendChild(td);
             }
             const actionTd = document.createElement('td');
             const addButton = document.createElement('button');
-            addButton.textContent = ` Add${element.playerName} to Current Team`;
-            addButton.onclick = () => addPlaerToTeam(element);
+            addButton.innerHTML = ` Add${element.playerName} ${"<br> </br>"} to Current Team`;
+            addButton.onclick = () => addPlaerToTeam(player);
             actionTd.appendChild(addButton);
             tr.appendChild(actionTd);
             tableBody.appendChild(tr);
         });
     }
 }
-function addPlaerToTeam(player) {
-    if (player) {
-        localStorage.setItem(`${player.position}`, JSON.stringify(player));
+function addPlaerToTeam(targetedPlayer) {
+    if (targetedPlayer) {
+        localStorage.setItem(`${targetedPlayer.position}`, JSON.stringify(targetedPlayer));
     }
-    addToCardDiv(player);
+    addToCardDiv(targetedPlayer === null || targetedPlayer === void 0 ? void 0 : targetedPlayer.position);
 }
-function addToCardDiv(player) {
-    if (player) {
-        const cardPlayerdiv = document.getElementById(`${player === null || player === void 0 ? void 0 : player.position}`);
-        cardPlayerdiv.innerHTML = "";
-        console.log(ALL_PROPERTIES1);
-        for (let i = 0; i < ALL_PROPERTIES1.length; i++) {
-            if (i == 1) {
-                continue;
+function addToCardDiv(position) {
+    // const listPositions: string[] = [""]
+    if (position) {
+        const cardPlayerdiv = document.getElementById(`${position}`);
+        if (cardPlayerdiv.childNodes.length > 0) {
+            cardPlayerdiv.innerHTML = "";
+        }
+        //    let p = document.createElement('p') as HTMLParagraphElement;
+        //    p.textContent = `${targetedPlayer[ALL_PROPERTIES[3]]}`
+        //    p.classList.add('title-Position');
+        //    cardPlayerdiv.appendChild(p);
+        let targetedPlayer = getDataLocalStorage(position);
+        if (targetedPlayer) {
+            for (let i = 0; i < ALL_PROPERTIES1.length; i++) {
+                if (i == 3) {
+                    continue;
+                }
+                ;
+                let p = document.createElement('p');
+                if (i == 0) {
+                    p.textContent = `${ALL_PROPERTIES1[4]}: ${targetedPlayer[ALL_PROPERTIES1[4]]} `;
+                }
+                else if (i == 1 || i == 2) {
+                    p.textContent = `${ALL_PROPERTIES1[i - 1]}: ${targetedPlayer[ALL_PROPERTIES1[i - 1]]}% `;
+                }
+                else {
+                    p.textContent = `${ALL_PROPERTIES1[i - 2]}: ${targetedPlayer[ALL_PROPERTIES1[i - 2]]} `;
+                }
+                cardPlayerdiv.appendChild(p);
             }
-            ;
-            let p = document.createElement('p');
-            if (i == 0) {
-                p.textContent = `${ALL_PROPERTIES1[4]}: ${player[ALL_PROPERTIES1[4]]} `;
-            }
-            else if (i == 2 || i == 3) {
-                p.textContent = `${ALL_PROPERTIES1[i - 2]}: ${player[ALL_PROPERTIES1[i - 2]]}% `;
-            }
-            else {
-                p.textContent = `${ALL_PROPERTIES1[i - 2]}: ${player[ALL_PROPERTIES1[i]]} `;
-            }
-            cardPlayerdiv.appendChild(p);
         }
     }
 }
+function renderDiVCards() {
+    for (let i = 0; i < positions.length; i++) {
+        addToCardDiv(positions[i]);
+    }
+}
+renderDiVCards();
--- script.ts
+++ script.ts
@@ -1,11 +1,25 @@
-const position = document.getElementById('selectPosition"')as HTMLSelectElement;
-const Points = document.getElementById("inputTotalPoints") as HTMLInputElement;
-const threePointes = document.getElementById("input3Pointers") as HTMLInputElement;
-const twoPointers = document.getElementById("input2Pointers") as HTMLInputElement;
 
 const submitButton = document.getElementById('searchPlayers') as HTMLInputElement;
-const BASE_URL: string = "https://nbaserver-q21u.onrender.com/api/filter";
+const BASE_URL: string = "https://nbaserver-q21u.onrender.com/api/";
+// const Bonus_Url: string = "localhost:8000/api/AddTeam"
 submitButton.addEventListener('click', postPlayers);
+const addTeamButton = document.getElementById('addTeamButton') as HTMLButtonElement;
+const positions: string[] = ["PG", "SG","SF","PF","C"]
+addTeamButton?.addEventListener('click', addTeam) 
+
+function apiMessage(msg: any){
+    let div = document.getElementById("apiMessage") as HTMLDivElement;
+    if(msg == 200){
+        // div.innerText = `${y}`
+    }
+    div.innerText = msg;
+    div.style.backgroundColor = "green";
+
+    setTimeout(function(){
+       div.innerHTML = '';
+       div.style.backgroundColor =''
+    }, 1000);
+}
 
 interface player{
 
@@ -14,6 +28,41 @@ twoPercent: number;
 threePercent: number;
 points: number;
 playerName?: string;
+}
+async function addTeam(){
+        const teamPlayers: any = {
+            players: []
+        }
+        for(let i: number =0; i< positions.length; i++){
+            let data = getDataLocalStorage(positions[i]);
+            teamPlayers.players.push(data);
+        }
+        try{
+            let data: any = await fetch(`${BASE_URL}AddTeam`,{
+                method: "POST",
+                headers: {'content-Type' : 'application/json'},
+                body: JSON.stringify(teamPlayers)
+            });
+            console.log(data)
+            let res: any =  await data.json();
+                 renderPlayers(res)
+                  apiMessage(data.status);
+                 return res
+        }
+        catch(err){
+            console.log(err)
+        }
+        localStorage.clear()
+        renderDiVCards()
+
+}
+function getDataLocalStorage(position: string): player|undefined{
+    let data : any = localStorage.getItem(position)
+    if(typeof data === 'string'){
+        const player: player = JSON.parse(data) || []
+        return player
+    }
+   
 }
 async function postPlayers(event:any):Promise<player[]|undefined> {
     event?.preventDefault();
@@ -27,16 +76,18 @@ async function postPlayers(event:any):Promise<player[]|undefined> {
         threePercent: arr[1],
         points: arr[0]
     }
-    console.log(searchFactors);
+   
     try{
-        let data: any = await fetch(BASE_URL,{
+        let data: any = await fetch(`${BASE_URL}filter`,{
             method: "POST",
             headers: {'content-Type' : 'application/json'},
             body: JSON.stringify(searchFactors)
+           
         });
-        let res: any =  await data.json();
-             console.log(res)
+        let res: any =  await data.json();          
+             apiMessage(data.status)
              renderPlayers(res)
+             return res
     }
     catch(err){
         console.log(err)
@@ -63,6 +114,7 @@ function renderPlayers(players: player[]| undefined){
 
     if(players){
         players.forEach(element =>{
+            const player: Partial<player> = {}
             let tr = document.createElement('tr') as HTMLTableRowElement;
             for(let i: number = 0; i < ALL_PROPERTIES.length; i++){
                 let td = document.createElement('td') as HTMLTableCellElement;
@@ -73,44 +125,62 @@ function renderPlayers(players: player[]| undefined){
                 else if(typeof val === 'number'){
                     td.innerText = val.toString();
                 }
+                player[ALL_PROPERTIES[i]] = val;
                 tr.appendChild(td)
                      }
                 const actionTd = document.createElement('td') as HTMLTableCellElement;
                 const addButton = document.createElement('button') as HTMLButtonElement;
-                addButton.textContent = ` Add${element.playerName} to Current Team`;
-                addButton.onclick = () => addPlaerToTeam(element);
+                addButton.innerHTML = ` Add${element.playerName} ${"<br> </br>"} to Current Team`;
+                addButton.onclick = () => addPlaerToTeam(player);
                 actionTd.appendChild(addButton);
                 tr.appendChild(actionTd);
                 tableBody.appendChild(tr);
         })
     }
 }
-function addPlaerToTeam(player: player|undefined){
-    if(player){
-        localStorage.setItem(`${player.position}`,JSON.stringify(player))
+function addPlaerToTeam(targetedPlayer: Partial<player>|undefined){
+    if(targetedPlayer){
+        localStorage.setItem(`${targetedPlayer.position}`,JSON.stringify(targetedPlayer))
     }
-    addToCardDiv(player)
+    addToCardDiv(targetedPlayer?.position)
 }
-function addToCardDiv(player: player|undefined){
-    if(player){
-   const cardPlayerdiv = document.getElementById(`${player?.position}`) as HTMLDivElement;
-   cardPlayerdiv.innerHTML =""
-   console.log(ALL_PROPERTIES1)
+function addToCardDiv(position: string | undefined){
+    // const listPositions: string[] = [""]
+    if(position){
+   const cardPlayerdiv = document.getElementById(`${position}`) as HTMLDivElement;
+   if(cardPlayerdiv.childNodes.length > 0){
+    cardPlayerdiv.innerHTML =""
+   }
+    
+//    let p = document.createElement('p') as HTMLParagraphElement;
+//    p.textContent = `${targetedPlayer[ALL_PROPERTIES[3]]}`
+//    p.classList.add('title-Position');
+//    cardPlayerdiv.appendChild(p);
+   let targetedPlayer = getDataLocalStorage(position);
+   if(targetedPlayer){
+   
     for(let i: number = 0; i< ALL_PROPERTIES1.length; i++){
-        if(i == 1){continue};
+        if(i == 3){continue};
         let p = document.createElement('p') as HTMLParagraphElement;
-        if(i == 0){ p.textContent = `${ALL_PROPERTIES1[4]}: ${player[ALL_PROPERTIES1[4]]} ` 
+        if(i == 0){ p.textContent = `${ALL_PROPERTIES1[4]}: ${targetedPlayer[ALL_PROPERTIES1[4]]} ` 
     }
-        else if( i == 2 || i == 3){
-             p.textContent = `${ALL_PROPERTIES1[i-2]}: ${player[ALL_PROPERTIES1[i-2]]}% `
+        else if( i == 1 || i == 2){
+             p.textContent = `${ALL_PROPERTIES1[i -1]}: ${targetedPlayer[ALL_PROPERTIES1[i-1]]}% `
         }else{
-            p.textContent = `${ALL_PROPERTIES1[i-2]}: ${player[ALL_PROPERTIES1[i]]} `
+            p.textContent = `${ALL_PROPERTIES1[i-2]}: ${targetedPlayer[ALL_PROPERTIES1[i-2]]} `
         }
         
-        cardPlayerdiv.appendChild(p);
+        cardPlayerdiv.appendChild(p);}
     
     }
 }
 }
 
 
+function renderDiVCards(){
+   
+    for(let i: number = 0; i< positions.length; i++){
+        addToCardDiv(positions[i]);
+    }
+}
+renderDiVCards();
